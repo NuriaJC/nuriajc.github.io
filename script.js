4114ba5562d22f1124a4c0c5ac9eaaa3adf96649
NuriaJC <82517863+NuriaJC@users.noreply.github.com>
2021-05-21 09:38:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -393,7 +393,7 @@ class Survival {
 //             return true;
         }
         else {
-            if ( this.globalEventsIndex > this.globalEvents.length ) {
+            if ( this.globalEventsIndex > 2 ) {
                 this.globalEventsIndex = 0;
                 this.globalEvents = shuffle(this.globalEvents);
             }
